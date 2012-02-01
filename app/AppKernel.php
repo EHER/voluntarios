76c2b3aa4caf3ff100fcaf02bf92707daec30198
Eher <alexandre@eher.com.br>
2012-01-31 23:32:04 -0200

    Tentativa de arrumar as dependências

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new Eher\VoluntariosBundle\EherVoluntariosBundle(),
         );
 
