766598bf119a3812df52bdef44f682f3de29f7df
Eher <alexandre@eher.com.br>
2012-06-04 00:39:15 -0300

    Layout using twitter bootstrap.

--- src/Eher/VoluntariosBundle/Controller/VoluntariosController.php
+++ src/Eher/VoluntariosBundle/Controller/VoluntariosController.php
@@ -12,42 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller,
 
 class VoluntariosController extends Controller
 {
-    private $navUrl = array();
-
-    public function generateNavUrl()
-    {
-        $this->navUrl['homepage'] = $this->get('router')->generate("homepage");
-        $this->navUrl['sobre'] = $this->get('router')->generate("sobre");
-        $this->navUrl['buscar'] = $this->get('router')->generate("buscar");
-        $this->navUrl['cadastrarVoluntario'] = $this->get('router')->generate("cadastrarVoluntario");
-        $this->navUrl['cadastrarEntidade'] = $this->get('router')->generate("cadastrarEntidade");
-        $this->navUrl['contato'] = $this->get('router')->generate("contato");
-    }
-
     public function indexAction()
     {
-        $this->generateNavUrl();
-
-        return $this->render(
-            'EherVoluntariosBundle:Voluntarios:index.html.twig',
-            array(
-                'navUrl' => $this->navUrl,
-            )
-        );
+        return $this->render('EherVoluntariosBundle:Voluntarios:index.html.twig');
     }
 
     public function sobreAction()
     {
-        $this->generateNavUrl();
-        return $this->render(
-            'EherVoluntariosBundle:Voluntarios:sobre.html.twig',
-            array('navUrl' => $this->navUrl)
-        );
+        return $this->render('EherVoluntariosBundle:Voluntarios:sobre.html.twig');
     }
 
     public function buscarAction()
     {
-        $this->generateNavUrl();
         $search = null;
         $cityName = $this->getRequest()->query->get('cidade');
         $stateName = $this->getRequest()->query->get('uf');
@@ -75,7 +51,6 @@ class VoluntariosController extends Controller
         return $this->render(
             'EherVoluntariosBundle:Voluntarios:buscar.html.twig',
             array(
-                'navUrl' => $this->navUrl,
                 'cityName' => $cityName,
                 'stateName' => $stateName,
                 'search' => $search,
@@ -85,32 +60,16 @@ class VoluntariosController extends Controller
 
     public function cadastrarVoluntarioAction()
     {
-        $this->generateNavUrl();
-        return $this->render(
-            'EherVoluntariosBundle:Voluntarios:cadastrarVoluntario.html.twig',
-            array('navUrl' => $this->navUrl)
-        );
+        return $this->render('EherVoluntariosBundle:Voluntarios:cadastrarVoluntario.html.twig');
     }
 
     public function cadastrarEntidadeAction()
     {
-        $this->generateNavUrl();
-
-
-        return $this->render(
-            'EherVoluntariosBundle:Voluntarios:cadastrarEntidade.html.twig',
-            array(
-                'navUrl' => $this->navUrl,
-            )
-        );
+        return $this->render('EherVoluntariosBundle:Voluntarios:cadastrarEntidade.html.twig');
     }
 
     public function contatoAction()
     {
-        $this->generateNavUrl();
-        return $this->render(
-            'EherVoluntariosBundle:Voluntarios:contato.html.twig',
-            array('navUrl' => $this->navUrl)
-        );
+        return $this->render('EherVoluntariosBundle:Voluntarios:contato.html.twig');
     }
 }
