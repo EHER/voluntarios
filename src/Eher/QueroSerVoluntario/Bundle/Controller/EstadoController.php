85b461103300a6dacad2a36de0b8d97b12887941
Eher <alexandre.eher@apontador.com>
2012-08-05 16:33:39 -0300

    Listagem de estados do brasil

---
+++ src/Eher/QueroSerVoluntario/Bundle/Controller/EstadoController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Eher\QueroSerVoluntario\Bundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Eher\QueroSerVoluntario\Bundle\Entity\Estado;
+
+/**
+ * Estado controller.
+ *
+ */
+class EstadoController extends Controller
+{
+    /**
+     * Lists all Estado entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entities = $em->getRepository('EherQueroSerVoluntarioBundle:Estado')->findAll();
+
+        return $this->render('EherQueroSerVoluntarioBundle:Estado:index.html.twig', array(
+            'entities' => $entities
+        ));
+    }
+
+    /**
+     * Finds and displays a Estado entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('EherQueroSerVoluntarioBundle:Estado')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Estado entity.');
+        }
+
+        return $this->render('EherQueroSerVoluntarioBundle:Estado:show.html.twig', array(
+            'entity'      => $entity,
+        ));
+    }
+
+}
---
+++ src/Eher/QueroSerVoluntario/Bundle/Tests/Controller/EstadoControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Eher\QueroSerVoluntario\Bundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class EstadoControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Go to the list view
+        $crawler = $client->request('GET', '/estado/');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        // Go to the show view
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+    }
+    */
+}
