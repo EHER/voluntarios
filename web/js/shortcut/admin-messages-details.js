d31adb7032b0465ab3795d55c0cc7e2e82db514a
Eher <alexandre@eher.com.br>
2014-08-19 21:27:51 -0300

    Configure command+a and ctrl+a shortcuts to select message text

--- web/js/shortcut/admin-messages-details.js
+++ web/js/shortcut/admin-messages-details.js
@@ -1,10 +1,15 @@
-require(['mousetrap', 'url-navigation'], function (Mousetrap, urlNavigation) {
+require(['jquery', 'mousetrap', 'url-navigation'], function ($, Mousetrap, urlNavigation) {
     Mousetrap.bind('j', function () {
         window.location.search = urlNavigation.nextVoluntarioId(window.location.search);
     });
 
     Mousetrap.bind('k', function () {
         window.location.search = urlNavigation.previousVoluntarioId(window.location.search);
+    });
+
+    Mousetrap.bind(['command+a', 'ctrl+a'], function() {
+        $("#message").focus().select();
+        return false;
     });
 });
 
